ce56730224a72235912150d74627362fe09606b4
alanrezendeee <alanrezendeee@gmail.com>
2026-04-10 12:47:32 -0300

    feat: ajustes de SEO e melhorias no laytou

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   site: 'https://cashflowfy.com.br',
 
   env: {
